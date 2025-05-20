aa3aab81c374917943eeb694b5f8b79749407ec6
Naika Estriplet <naika.c.3113@gmail.com>
2025-05-20 17:01:36 -0400

    added some styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Article.destroy_all
+puts 'clearing database'
+
+10.times do
+  article = Article.new(
+    title: Faker::TvShows::BreakingBad.episode,
+    content: Faker::Lorem.paragraph(sentence_count: 8, supplemental: false, random_sentences_to_add: 10)
+  )
+  puts 'creating article'
+  article.save!
+end
